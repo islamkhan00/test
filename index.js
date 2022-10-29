0ce8e2cce2f2c35b99388e93a66cf038adb7b649
islamkhan00 <you@example.com>
2022-10-29 09:02:02 +0530

    pakakge

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import express from "express";
 import fs from "fs";
 import path from "path";
 
-import test from "./routes/test.route.js";
-import {user} from "./routes/user.route.js";
-import {category} from "./routes/category.route.js";
-import {subcategory} from "./routes/sub.category.route.js";
-import {product} from "./routes/product.route.js";
-import {reviewrating} from "./routes/review.rating.route.js";
+// import test from "./routes/test.route.js";
+// import {user} from "./routes/user.route.js";
+// import {category} from "./routes/category.route.js";
+// import {subcategory} from "./routes/sub.category.route.js";
+// import {product} from "./routes/product.route.js";
+// import {reviewrating} from "./routes/review.rating.route.js";
 import connectDB from "./config/db.js"
 
 
@@ -15,12 +15,12 @@ import connectDB from "./config/db.js"
 const app = express()
 app.use(express.json())
 connectDB();
-app.use(test);
-app.use(category);
-app.use(user);
-app.use(subcategory);
-app.use(product);
-app.use(reviewrating);
+// app.use(test);
+// app.use(category);
+// app.use(user);
+// app.use(subcategory);
+// app.use(product);
+// app.use(reviewrating);
 
 // app.use(router);
 app.listen(3002,(request,response) =>{
